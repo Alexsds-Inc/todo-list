44cd9f4fdf838158fa4d93186fccbce592a9edf4
Aleksandr Korol <korol1368@gmail.com>
2021-01-12 17:56:37 +0200

    ALT-4 fix unit test of TaskFormComponent

--- src/app/components/completed-page/completed-page.component.spec.ts
+++ src/app/components/completed-page/completed-page.component.spec.ts
@@ -1,7 +1,7 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 
 import {CompletedPageComponent} from './completed-page.component';
-import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import {TodoService} from '@shared/services/todo.service';
 
 describe('CompletedPageComponent', () => {
--- src/app/components/task-form/task-form.component.spec.ts
+++ src/app/components/task-form/task-form.component.spec.ts
@@ -1,14 +1,31 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogModule, MatDialogRef} from '@angular/material/dialog';
 
 import {TaskFormComponent} from './task-form.component';
 
 describe('AddTaskComponent', () => {
+  const mockDialogRef = {
+    close: jasmine.createSpy('close')
+  };
   let component: TaskFormComponent;
   let fixture: ComponentFixture<TaskFormComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [TaskFormComponent],
+      imports: [MatDialogModule],
+      providers: [
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: {}
+        },
+        {
+          provide: MatDialogRef,
+          useValue: mockDialogRef
+        }
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
   });
 
