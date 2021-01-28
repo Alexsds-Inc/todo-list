5cadc1d3c4bcf34212c55f757535c4a92bc1ab3c
Aleksandr Korol <korol1368@gmail.com>
2021-01-29 00:44:31 +0200

    get authorized user in ProfileComponent and templating his

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from '@shared/services/auth.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
+  name: string | undefined;
+  email: string | undefined;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private authService: AuthService) {
   }
 
+  ngOnInit(): void {
+    this.authService.getAuthorizedUser().subscribe((user) => {
+      if (!user) {
+        return;
+      }
+
+      this.name = user.name;
+      this.email = user.email;
+    });
+  }
 }
